555e6ea5828c135a1ae67a682456832975c4faee
Sable <sablechilds@yahoo.com>
2020-07-24 21:30:43 -0400

    mvp

---
+++ accounts/accounts-Router.js
@@ -0,0 +1,60 @@
+const express = require('express');
+
+const db = require('../data/dbConfig');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+db.select('*')
+.from('accounts')
+.then((accounts)=> res.status(200).json({data: accounts}))
+.catch((err)=> { console.log(err)})
+})
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+    db('accounts') // can't use select keyword
+    .where('id', id)
+    .first()
+    .then((account)=> res.status(200).json({data:account}))
+    .catch((err)=> console.log(err))
+})
+
+router.post('/', (req, res) => {
+    const accountData = req.body;
+    db('accounts')
+    .insert(accountData)
+    .then((id)=> res.status(201).json({data: id}))
+    .catch((err)=> console.log(err))
+})
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+   db('accounts')
+   .where('id', id) 
+   .update(changes)
+   .then((count)=> {
+       if(count > 0){
+           res.status(200).json({data:count})
+       } else {
+           res.status(404).json({message:'There was no record to update.'})
+       }
+   })
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    db('accounts')
+   .where('id', id) 
+   .del()
+   .then((count)=> {
+       if(count > 0){
+           res.status(200).json({data:count})
+       } else {
+           res.status(404).json({message:'There was no record to update.'})
+       }
+   })
+})
+
+module.exports = router;
